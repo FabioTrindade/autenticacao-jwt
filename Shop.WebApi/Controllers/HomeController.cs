5ed802722042cdf0441117b5cce645e28eb8722d
fabio.trindade <fabio.trindade@hotmail.com>
2021-10-24 10:56:06 -0400

    Add project files.

---
+++ Shop.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.WebApi.Models;
+using Shop.WebApi.Repositories;
+using Shop.WebApi.Services;
+using System.Threading.Tasks;
+
+namespace Shop.WebApi.Controllers
+{
+    [Route("v1/account")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Autenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.UserName, model.Password);
+            if (user is null)
+                return NotFound(new { message = "Usuário ou senha inválido" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("autenthicated")]
+        [Authorize]
+        public string Autenthicated() => string.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
